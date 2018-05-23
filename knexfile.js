26bf29a6a5d611555d35186a5b8e79d305ce60dd
Clark Newell <clarknewell@Clarks-MacBook-Pro.local>
2018-05-23 16:49:37 -0600

    almost complete

---
+++ knexfile.js
@@ -0,0 +1,10 @@
+module.exports = {
+  development: {
+    client: "pg",
+    connection: "postgres:///galvanize-memory3"
+  },
+  production: {
+    client: "pg",
+    connection: process.env.DATABASE_URL
+  }
+}
